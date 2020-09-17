1c181726f417564aaf70c4d42490f7c49e1649cc
pde-bakk <pde-bakk@student.codam.nl>
2020-09-17 18:46:46 +0200

    added compiler variability and colourcoded output

--- run.sh
+++ run.sh
@@ -6,22 +6,37 @@
 #    By: peerdb <peerdb@student.codam.nl>             +#+                      #
 #                                                    +#+                       #
 #    Created: 2020/09/09 16:47:13 by peerdb        #+#    #+#                  #
-#    Updated: 2020/09/16 22:38:48 by pde-bakk      ########   odam.nl          #
+#    Updated: 2020/09/17 18:46:01 by pde-bakk      ########   odam.nl          #
 #                                                                              #
 # **************************************************************************** #
 
 #!/bin/bash
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+PURPLE='\033[0;35m'
+LIGHTPURPLE='\033[1;35m'
+CYAN='\033[0;36m'
+LIGHTCYAN='\033[1;36m'
+RESET='\033[0m'
+# echo "Passed" $# "arguments to run.sh" 
+ARG=""
+if [[ $1 == "REALG" ]]; then
+	ARG="REALG=1"
+fi
 
-make re && ./containers.out > ft.txt
+make fuckingclean
+
+make re $ARG && ./containers.out > ft.txt
 
 sed "s/ft::/std::/g" main.cpp > stdmain.cpp
 
-make re STD=1 && ./containers.out > std.txt
+make re STD=1 $ARG && ./containers.out > std.txt
 
 diff ft.txt std.txt
 
 if [ $? -eq 1 ]; then
-	echo "Diff failed"
+	echo "${RED}Diff failed${RESET}"
 else
-	echo "Diff found no differences"
+	echo "${LIGHTPURPLE}Diff found no differences${RESET}"
 fi
