0a37af04efbbc33870f356a4c424c5d894140c94
Peer de Bakker <pde-bakk@student.codam.nl>
2020-09-26 14:57:53 +0200

    fixed a lil leak in my operator=

--- srcs/List/List.hpp
+++ srcs/List/List.hpp
@@ -6,7 +6,7 @@
 /*   By: pde-bakk <pde-bakk@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/09/06 12:23:59 by pde-bakk      #+#    #+#                 */
-/*   Updated: 2020/09/26 00:39:14 by peerdb        ########   odam.nl         */
+/*   Updated: 2020/09/26 14:56:37 by peerdb        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,7 @@
 # include "ListNode.hpp"
 # include "../Iterators/BidirectionalIterator.hpp"
 # include "../Traits.hpp"
+# include "../Extra.hpp"
 
 # if defined(unix) || defined(__unix__) || defined(__unix)
 #  define PEER_MAX SSIZE_MAX
@@ -30,37 +31,6 @@
 # endif
 
 namespace ft {
-	
-	template<typename Iterator>
-	ptrdiff_t distance(Iterator first, Iterator last,
-			typename enable_if<is_iterator<typename Iterator::iterator_category>::value, Iterator>::type * = 0) {
-		ptrdiff_t dist = 0;
-		while (first != last) {
-			++dist;
-			first++;
-		}
-		return dist;
-	}
-	template <class InputIterator, class Distance>
-	void advance (InputIterator& it, Distance n,
-			typename enable_if<is_iterator<typename InputIterator::iterator_category>::value, InputIterator>::type * = 0) {
-		while (n) {
-			if (n > 0) {
-				++it;
-				--n;
-			}
-			else {
-				--it;
-				++n;
-			}
-		}
-	}
-	template< typename S >
-	void	itemswap(S& var1, S& var2) {
-		S tmpvar = var1;
-		var1 = var2;
-		var2 = tmpvar;
-	}
 	
 	template < class T, class Alloc = std::allocator<T> >
 	class list {
@@ -125,8 +95,6 @@ namespace ft {
 		}
 		list&	operator=(const list& x) {
 			this->clear();
-			this->head = new node<T>();
-			this->tail = new node<T>();
 			this->head->next = this->tail;
 			this->tail->prev = this->head;
 			this->length = 0;
